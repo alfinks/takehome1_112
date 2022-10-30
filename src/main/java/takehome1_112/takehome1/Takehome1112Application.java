7ecbbbc99062e632ff3b467f376533f59c19291c
alfinks <maulanaafiansyah@gmail.com>
2022-10-30 12:51:14 +0700

    membuat projek

---
+++ src/main/java/takehome1_112/takehome1/Takehome1112Application.java
@@ -0,0 +1,13 @@
+package takehome1_112.takehome1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Takehome1112Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Takehome1112Application.class, args);
+	}
+
+}
---
+++ src/test/java/takehome1_112/takehome1/Takehome1112ApplicationTests.java
@@ -0,0 +1,13 @@
+package takehome1_112.takehome1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Takehome1112ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
